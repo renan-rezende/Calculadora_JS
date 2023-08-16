53ab0aa649e84bae494528b70b6c170b883b1a3c
Renan <112997019+Renan-Rezende@users.noreply.github.com>
2023-08-16 18:19:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,36 @@ function calculete() {
   const result = eval(input.value)
   resultInput.value = result
 }
+
+// ADICIONANDO FUNÇÃO QUE MUDA A COR DO TEMA DA PAGINA (DARK and LIGHT)
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+
+})
+
+// ADICIONANDO FUNÇÃO QUE COPIA O CONTEUDO DA SAÍDA PARA A AREA DE TRABALHO
+document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+  const button = ev.currentTarget
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!"
+    button.classList.add("success")
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    button.innerText = "Copy"
+    button.classList.remove("success")
+  }
+})
+
+
